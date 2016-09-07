3559b666d32287a1aade62554f0648ee064dbd35
chenxiaoyu <chxiyu0720@163.com>
2016-09-07 18:33:08 +0800

    spring.v1-2016-9-7

--- study-spring/src/com/chen/learn/bean/UserInfo.java
+++ study-spring/src/com/chen/learn/bean/UserInfo.java
@@ -1,6 +1,24 @@
 package com.chen.learn.bean;
 
+/**
+ * bean文件
+ * @author Administrator
+ *
+ */
 public class UserInfo {
 	private String username;
-	private String 
+	private String password;
+	public String getUsername() {
+		return username;
+	} 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
